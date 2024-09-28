bbed271ad234b2db78c4146adb7d7a8527cef585
Anmol-Kumar01 <anmol888236@gmail.com>
2024-09-28 09:55:14 +0530

    error fixed

--- projectU/src/app/page.js
+++ projectU/src/app/page.js
@@ -82,7 +82,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="https://in.images.search.yahoo.com/images/view;_ylt=AwrKGkTd0PdmrkYz8Q29HAx.;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzI2MjU0MWU2NzhjMWEzZTQyMTMzYTYyNGE1YTY5ZDNmBGdwb3MDNQRpdANiaW5n?back=https%3A%2F%2Fin.images.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dnext%2Bjs%26type%3DE210IN714G0%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D5&w=1920&h=1357&imgurl=codewithmosh.com%2F_next%2Fimage%3Furl%3Dhttps%3A%252F%252Fcdn.filestackcontent.com%252F8MbtJ4hTAaOk3KPcptqZ%26w%3D3840%26q%3D75&rurl=https%3A%2F%2Fcodewithmosh.com%2Fp%2Fultimate-nextjs-series&size=67KB&p=next+js&oid=262541e678c1a3e42133a624a5a69d3f&fr2=piv-web&fr=mcafee&tt=The+Ultimate+Next.js+Series&b=0&ni=21&no=5&ts=&tab=organic&sigr=zNSoad6Q8rVl&sigb=yGngBvG3NpvB&sigi=fEy.9cUznXH7&sigt=o7wf57Qtx.gK&.crumb=dxjjA23/ikJ&fr=mcafee&fr2=piv-web&type=E210IN714G0"
+            src="https://nextjs.org/icons/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
